07875a774da043e0f9ede862e310890df65a713e
Austin Young <austineyoung2000@gmail.com>
2025-02-10 17:33:24 -0500

    shusky: fixed v4a sepolicy

--- sepolicy/husky-sepolicy.mk
+++ sepolicy/husky-sepolicy.mk
@@ -1,5 +1,8 @@
 # sepolicy exclusively for husky.
 BOARD_SEPOLICY_DIRS += device/google/shusky/sepolicy/husky
 
+# audio
+BOARD_SEPOLICY_DIRS += device/google/shusky/sepolicy/audio
+
 # unresolved SELinux error log with bug tracking
 BOARD_SEPOLICY_DIRS += device/google/shusky/sepolicy/tracking_denials
--- sepolicy/shiba-sepolicy.mk
+++ sepolicy/shiba-sepolicy.mk
@@ -1,5 +1,8 @@
 # sepolicy exclusively for shiba.
 BOARD_SEPOLICY_DIRS += device/google/shusky/sepolicy/shiba
 
+# audio
+BOARD_SEPOLICY_DIRS += device/google/shusky/sepolicy/audio
+
 # unresolved SELinux error log with bug tracking
 BOARD_SEPOLICY_DIRS += device/google/shusky/sepolicy/tracking_denials

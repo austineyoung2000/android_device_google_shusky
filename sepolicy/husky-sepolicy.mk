53ddc87ee4f6e5e2c4837f348346ac33c2adae0e
Austin Young <austineyoung2000@gmail.com>
2025-04-18 23:28:53 -0400

    Revert "shusky: fixed v4a sepolicy"
    This reverts commit 07875a774da043e0f9ede862e310890df65a713e.

--- sepolicy/husky-sepolicy.mk
+++ sepolicy/husky-sepolicy.mk
@@ -1,9 +1,6 @@
 # sepolicy exclusively for husky.
 BOARD_SEPOLICY_DIRS += device/google/shusky/sepolicy/husky
 
-# audio
-BOARD_SEPOLICY_DIRS += device/google/shusky/sepolicy/audio
-
 # unresolved SELinux error log with bug tracking
 BOARD_SEPOLICY_DIRS += device/google/shusky/sepolicy/tracking_denials
 
--- sepolicy/shiba-sepolicy.mk
+++ sepolicy/shiba-sepolicy.mk
@@ -1,9 +1,6 @@
 # sepolicy exclusively for shiba.
 BOARD_SEPOLICY_DIRS += device/google/shusky/sepolicy/shiba
 
-# audio
-BOARD_SEPOLICY_DIRS += device/google/shusky/sepolicy/audio
-
 # unresolved SELinux error log with bug tracking
 BOARD_SEPOLICY_DIRS += device/google/shusky/sepolicy/tracking_denials
 
